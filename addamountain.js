bff46edf8fb956d943676575aa958e063fbb1538
JRProgramming <ramirezjohnny@icloud.com>
2018-01-11 13:20:29 -0500

    Update addamountain.js

--- addamountain.js
+++ addamountain.js
@@ -1,5 +1,21 @@
-var priceOf7to17 = document.getElementById("$7-17")
+var priceOf6AndUnder = document.getElementById("$6AndUnder").innerHTML
+var priceOf7to17 = document.getElementById("$7-17").innerHTML
+var priceof18to69 = document.getElementById("$18-69").innerHTML
+var priceOfSenior = document.getElementById("$Senior").innerHTML
 function addAMountain()
 {
+alert(priceOf6AndUnder)
 alert(priceOf7to17)
+alert(priceOf18to69)
+alert(priceOfSenior)
+var save = firebase.database().ref('Mountain/').push({
+    priceOf6AndUnder: priceOf6AndUnder,
+    priceOf7to17: priceOf7to17,
+    priceOf18to69: priceOf18to69,
+    priceOfSenior: priceOfSenior
+  });
+var id = save.key
+firebase.database().ref('user/' + id).update({
+identification: id
+})
 }
