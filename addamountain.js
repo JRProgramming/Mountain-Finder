0a9477265c8802f3080f2e5c0fa96ade18aea986
JRProgramming <ramirezjohnny@icloud.com>
2018-01-11 13:30:32 -0500

    Update addamountain.js

--- addamountain.js
+++ addamountain.js
@@ -2,12 +2,22 @@ var priceOf6AndUnder = document.getElementById("$6AndUnder").value
 var priceOf7to17 = document.getElementById("$7-17").value
 var priceof18to69 = document.getElementById("$18-69").value
 var priceOfSenior = document.getElementById("$Senior").value
+var childFriendly = document.getElementByName("CF")
+var childFriendlySelected;
 function addAMountain()
 {
 alert(priceOf6AndUnder)
 alert(priceOf7to17)
 alert(priceOf18to69)
 alert(priceOfSenior)
+for(i=0;i<childFriendly.length;i++)
+{
+    if(childFriendly[i].checked)
+    {
+        childFriendlySelected = childFriendly[i].value
+    }
+}
+alert(childFriendlySelected)
 var save = firebase.database().ref('Mountain/').push({
     priceOf6AndUnder: priceOf6AndUnder,
     priceOf7to17: priceOf7to17,
