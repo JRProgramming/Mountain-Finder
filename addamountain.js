a5c90b508663295022635ed0bcb6c290d636356d
JRProgramming <ramirezjohnny@icloud.com>
2018-01-11 14:09:25 -0500

    Update addamountain.js

--- addamountain.js
+++ addamountain.js
@@ -1,21 +1,14 @@
+function addAMountain()
+{
 var priceOf6AndUnder = document.getElementById("$6AndUnder").value
 var priceOf7to17 = document.getElementById("$7>17").value
 var priceOf18to69 = document.getElementById("$18>69").value
 var priceOfSenior = document.getElementById("$Senior").value
 var childFriendly = document.getElementByName("CF")
-var childFriendlySelected;
-function addAMountain()
-{
 alert(priceOf6AndUnder)
 alert(priceOf7to17)
 alert(priceOf18to69)
 alert(priceOfSenior)
-for(i=0;i<childFriendly.length;i++)
-{
-    if(childFriendly[i].checked)
-    {
-        childFriendlySelected = childFriendly[i].value
-    }
-}
+
 alert(childFriendlySelected)
 }
