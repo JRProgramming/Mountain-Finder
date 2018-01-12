54fe047dbc97ec782542c3160a152c668a2c1938
JRProgramming <ramirezjohnny@icloud.com>
2018-01-12 12:49:16 -0500

    Update addamountain.js

--- addamountain.js
+++ addamountain.js
@@ -4,11 +4,15 @@ var priceOf6AndUnder = document.getElementById("$6AndUnder").value
 var priceOf7to17 = document.getElementById("$7>17").value
 var priceOf18to69 = document.getElementById("$18>69").value
 var priceOfSenior = document.getElementById("$Senior").value
-var rating = document.getElementsByName("CF")
-for(i=0;i<rating.length;i++)
+var childRating = document.getElementsByName("CF")
+for(i=0;i<childRating.length;i++)
 {
-    alert(rating[i])
+    if(childRating[i].checked)
+    {
+     var checked = childRating[i].value
+    }
 }
+alert(checked)
 var save = firebase.database().ref('Mountain/').push({
     priceOf6AndUnder: priceOf6AndUnder,
     priceOf7to17: priceOf7to17,
