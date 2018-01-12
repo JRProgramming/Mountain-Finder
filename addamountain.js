1c8bdd7ce9528b084322f6576687930dbb68813d
JRProgramming <ramirezjohnny@icloud.com>
2018-01-12 10:02:16 -0500

    Update addamountain.js

--- addamountain.js
+++ addamountain.js
@@ -4,10 +4,6 @@ var priceOf6AndUnder = document.getElementById("$6AndUnder").value
 var priceOf7to17 = document.getElementById("$7>17").value
 var priceOf18to69 = document.getElementById("$18>69").value
 var priceOfSenior = document.getElementById("$Senior").value
-alert(priceOf6AndUnder)
-alert(priceOf7to17)
-alert(priceOf18to69)
-alert(priceOfSenior)
 var save = firebase.database().ref('Mountain/').push({
     priceOf6AndUnder: priceOf6AndUnder,
     priceOf7to17: priceOf7to17,
