1aa287c71197ade8ef62012754a1f0bb751ca654
JRProgramming <ramirezjohnny@icloud.com>
2018-01-18 14:03:24 -0500

    Update addamountain.js

--- addamountain.js
+++ addamountain.js
@@ -1,9 +1,13 @@
 function addAMountain()
 {
+var mountainName = document.getElementById("mtnName").value;
+var mountainLocation = document.getElementById("locationOfTheMountain").value;
 var priceOf6AndUnder = document.getElementById("$6AndUnder").value;
 var priceOf7to17 = document.getElementById("$7>17").value;
 var priceOf18to69 = document.getElementById("$18>69").value;
 var priceOfSenior = document.getElementById("$Senior").value;
+var skiRentalPrice = document.getElementById("skiPrice").value;
+var snowboardRentalPrice = document.getElementById("snowboardPrice").value;
 var childRating = document.getElementsByName("CF");
 var amountOfSnow = document.getElementsByName("SQ");
 var foodQuality = document.getElementsByName("FQ");
@@ -25,13 +29,17 @@ for(var i=0;i<childRating.length;i++)
 if(crChecked != null && aSChecked != null && fQChecked != null)
     {
     var save = firebase.database().ref('Mountain/').push({
+        MountainName : mountainName,
+        MountainLocation: mountainLocation,
         priceOf6AndUnder: priceOf6AndUnder,
         priceOf7to17: priceOf7to17,
         priceOf18to69: priceOf18to69,
         priceOfSenior: priceOfSenior,
         ChildFriendly: crChecked,
         AmountOfSnow: aSChecked,
-        FoodQuality: fQChecked
+        FoodQuality: fQChecked,
+        SkiRentalPrice: skiRentalPrice,
+        SnowboardRentalPrice: snowboardRentalPrice
     });
     var id = save.key
         firebase.database().ref('Mountain/' + id).update({
