9bb28aab7481906f7da4d3ad049183e32b47f871
JRProgramming <ramirezjohnny@icloud.com>
2018-03-15 13:56:14 -0400

    Update mountain.js

--- mountain.js
+++ mountain.js
@@ -1,5 +1,22 @@
 var mountains = []
 window.onload = function()
 {
-   alert("hi")
+    var updateData = firebase.database().ref("Mountain");
+    updateData.on("child_added", function(data, prevChildKey) {
+    var data = data.val() 
+    mountains.push(data)
+    })
+   firebase.database().ref('Mountains/').set("Data is logged", function(error) {
+            if (error) {
+                  alert("Data could not be saved." + error);
+            } else {
+      for(var i=0;i<mountains.length;i++)
+      {
+         if(mountains[i] == sessionStorage.getItem("mountainName"))
+         {
+            document.getElementById("mountainName").innerHTML = mountains[i].MountainName
+         }
+      }
+     }
+   })
 }
